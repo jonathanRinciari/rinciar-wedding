086e3616985ba92a5d8e48ac300c4711b180d7ca
Jon Rinciari <rinciar@amazon.com>
2023-01-22 10:09:23 -0800

    initial

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,18 +5,9 @@ const config: GatsbyConfig = {
     title: `rinciari-wedding-photos`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+    'gatsby-plugin-svgr'
+  ]
 };
 
 export default config;
---
+++ src/pages/components/polarid.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Polarid = ({children}: React.PropsWithChildren) => {
+
+    return (
+        <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <filter id="filter2912">
+            <feGaussianBlur id="feGaussianBlur2914" stdDeviation="3.4782385"></feGaussianBlur>
+          </filter>
+        </defs>
+        <g id="layer1" transform="matrix(1, 0, 0, 1, -327.929779, -289.603241)">
+          <g id="g3271" transform="translate(9.9102 9.3779)">
+            <path id="rect2893" filter='url(#filter2912)' fill="#000000" d="m413.06 322.28h330.18v365.47s-82.55-7.18-165.09-7.18c-82.55 0-165.09 7.18-165.09 7.18v-365.47z"></path>
+            <rect id="rect2889" fill="#f2f2f2"  height="362.18" width="330.18" y="318.05" x="413.06"></rect>
+            <rect id="rect2916" opacity=".55556" fill="#cccccc" height="308.62" width="306.51" y="328.2" x="424.89"></rect>
+            <rect id="rect2310" strokeLinejoin='round' stroke='rgb(0, 0, 0)' stroke-width="0.78245" fill="rgb(0, 0, 0)" opacity="0" height="300.34" width="300.34" y="331.14" x="427.98"></rect>
+            <path id="rect3250" opacity="0.07" fill="rgb(255, 255, 255)" d="m427.59 330.75h300.34s-47.35 61.4-122.43 102.97c-75.09 41.58-177.91 63.33-177.91 63.33v-166.3z"></path>
+            {/* <image height="300.34" width="300.34" y="331.14" x="427.98" href={url} /> */}
+            <foreignObject height="308.62" width="306.51" y="328.2" x="424.89">
+                {children}
+            </foreignObject>
+          </g>
+        </g>
+      </svg> 
+    );
+}
